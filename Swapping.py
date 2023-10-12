0b8c67e8804bc121d109d17127f95630bdc3fc9e
patil1604 <147031061+patil1604@users.noreply.github.com>
2023-10-12 11:40:39 +0530

    Add files via upload

---
+++ Swapping.py
@@ -0,0 +1,12 @@
+#Program to swap two numbers
+x = input("Enter the value of x: ")
+y = input("Enter the value of y: ")
+print("Before swapping")
+print("x = ",x)
+print("y = ",y)
+temp = x
+x = y
+y = temp
+print("After swapping")
+print("x = ",x)
+print("y = ",y)
